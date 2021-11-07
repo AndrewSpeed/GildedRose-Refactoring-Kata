4afb9c9a4b065924093f73c22247bf3989adc512
Andrew Speed <andrew.cspeed@gmail.com>
2021-11-07 15:41:34 +0000

    Add seam for normal item and remove old implementation

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -31,6 +31,16 @@ class GildedRose(object):
         
         return item
 
+    def update_normal_item(self, item):
+        if item.quality > 0:
+            if item.sell_in > 0:
+                item.quality -= 1
+            else:
+                item.quality -= 2
+        item.sell_in -=1
+
+        return item
+
     def update_sulfuras(self, item):
         return item
 
@@ -42,35 +52,7 @@ class GildedRose(object):
                 return self.update_backstage_pass(item)
             if item.name == "Sulfuras, Hand of Ragnaros":
                 return self.update_sulfuras(item)
-            else:
-                if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                    if item.quality > 0:
-                        if item.name != "Sulfuras, Hand of Ragnaros":
-                            item.quality = item.quality - 1
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
-                        if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                            if item.sell_in < 11:
-                                if item.quality < 50:
-                                    item.quality = item.quality + 1
-                            if item.sell_in < 6:
-                                if item.quality < 50:
-                                    item.quality = item.quality + 1
-                if item.name != "Sulfuras, Hand of Ragnaros":
-                    item.sell_in = item.sell_in - 1
-                if item.sell_in < 0:
-                    if item.name != "Aged Brie":
-                        if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                            if item.quality > 0:
-                                if item.name != "Sulfuras, Hand of Ragnaros":
-                                    item.quality = item.quality - 1
-                        else:
-                            item.quality = item.quality - item.quality
-                    else:
-                        if item.quality < 50:
-                            item.quality = item.quality + 1
-
+            return self.update_normal_item(item)
 
 class Item:
     def __init__(self, name, sell_in, quality):
