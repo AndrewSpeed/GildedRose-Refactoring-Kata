0d4c30b30e1d5197ceafc93df9f5cac66e30ff84
Andrew Speed <andrew.cspeed@gmail.com>
2021-11-07 15:38:13 +0000

    Add seam for backstage passes

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -5,6 +5,21 @@ class GildedRose(object):
     def __init__(self, items):
         self.items = items
 
+    def update_backstage_pass(self, item):
+        if item.quality < 50:
+            if item.sell_in <= 0:
+                item.quality = 0
+            elif item.sell_in <= 5:
+                item.quality += 3
+            elif item.sell_in <= 10:
+                item.quality += 2
+            else:
+                item.quality += 1
+
+        item.sell_in -= 1
+
+        return item
+
     def update_brie(self, item):
         if item.quality < 50:
             if item.sell_in > 0:
@@ -23,6 +38,8 @@ class GildedRose(object):
         for item in self.items:
             if item.name == "Aged Brie":
                 return self.update_brie(item)
+            if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                return self.update_backstage_pass(item)
             if item.name == "Sulfuras, Hand of Ragnaros":
                 return self.update_sulfuras(item)
             else:
