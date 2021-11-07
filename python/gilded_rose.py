c0d3104b5813156a54790ad7f5f38993b702f1e8
Andrew Speed <andrew.cspeed@gmail.com>
2021-11-07 18:27:50 +0000

    Refactor and implement conjured item

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -22,48 +22,69 @@ class ItemProcessor:
     def __init__(self, item):
         self.item = item
 
-    def update(self):
-        self.update_quality()
-        self.update_sell_in()
+    def has_expired(self):
+        return self.item.sell_in > 0
+
+    def update_quality_before_expiry(self):
+        self.item.quality -= 1
+
+    def update_quality_after_expiry(self):
+        self.item.quality -= 2
 
     def update_quality(self):
-        if self.item.quality > 0:
-            if self.item.sell_in > 0:
-                self.item.quality -= 1
+        if self.item.quality > 0 and self.item.quality < 50:
+            if self.has_expired():
+                self.update_quality_before_expiry()
             else:
-                self.item.quality -= 2
+                self.update_quality_after_expiry()
 
     def update_sell_in(self):
         self.item.sell_in -= 1
 
+    def update(self):
+        self.update_quality()
+        self.update_sell_in()
+
 class AgedBrieProcessor(ItemProcessor):
-    def update_quality(self):
-        if self.item.quality < 50:
-            if self.item.sell_in > 0:
-                self.item.quality += 1
-            else:
-                self.item.quality += 2
+    """Item that increases in quality"""
+    def update_quality_before_expiry(self):
+        self.item.quality += 1
+
+    def update_quality_after_expiry(self):
+        self.item.quality += 2
 
 class BackstagePassProcessor(ItemProcessor):
-    def update_quality(self):
-        if self.item.quality < 50:
-            if self.item.sell_in <= 0:
-                self.item.quality = 0
-            elif self.item.sell_in <= 5:
-                self.item.quality += 3
-            elif self.item.sell_in <= 10:
-                self.item.quality += 2
-            else:
-                self.item.quality += 1
+    """Item that increases in quality until it expires, then drops to zero"""
+    def update_quality_before_expiry(self):
+        if self.item.sell_in <= 5:
+            self.item.quality += 3
+        elif self.item.sell_in <= 10:
+            self.item.quality += 2
+        else:
+            self.item.quality += 1
+
+    def update_quality_after_expiry(self):
+        self.item.quality = 0
+
+class ConjuredProcessor(ItemProcessor):
+    """Item that degrades in quality twice as fast as normal items"""
+    def update_quality_before_expiry(self):
+        self.item.quality -= 2
+
+    def update_quality_after_expiry(self):
+        self.item.quality -= 4
+
 
 class SulfurasProcessor(ItemProcessor):
     def update(self):
+        # item does not have to be sold, or decrease in value
         pass
 
 class ProcessorFactory:
     item_to_processor_mapping = {
         "Aged Brie": AgedBrieProcessor,
         "Backstage passes to a TAFKAL80ETC concert": BackstagePassProcessor,
+        "Conjured Mana Cake": ConjuredProcessor,
         "Sulfuras, Hand of Ragnaros": SulfurasProcessor,
     }
 
