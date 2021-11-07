a41afff07110b41eda95896724b4fa83d053a108
Andrew Speed <andrew.cspeed@gmail.com>
2021-11-07 18:02:46 +0000

    Extract update functions into processor objects

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,58 +1,13 @@
 # -*- coding: utf-8 -*-
 
 class GildedRose(object):
-
     def __init__(self, items):
         self.items = items
 
-    def update_backstage_pass(self, item):
-        if item.quality < 50:
-            if item.sell_in <= 0:
-                item.quality = 0
-            elif item.sell_in <= 5:
-                item.quality += 3
-            elif item.sell_in <= 10:
-                item.quality += 2
-            else:
-                item.quality += 1
-
-        item.sell_in -= 1
-
-        return item
-
-    def update_brie(self, item):
-        if item.quality < 50:
-            if item.sell_in > 0:
-                item.quality += 1
-            else:
-                item.quality += 2
-
-        item.sell_in -= 1
-        
-        return item
-
-    def update_normal_item(self, item):
-        if item.quality > 0:
-            if item.sell_in > 0:
-                item.quality -= 1
-            else:
-                item.quality -= 2
-        item.sell_in -=1
-
-        return item
-
-    def update_sulfuras(self, item):
-        return item
-
     def update_quality(self):
         for item in self.items:
-            if item.name == "Aged Brie":
-                return self.update_brie(item)
-            if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                return self.update_backstage_pass(item)
-            if item.name == "Sulfuras, Hand of Ragnaros":
-                return self.update_sulfuras(item)
-            return self.update_normal_item(item)
+            processor = ProcessorFactory.for_item(item)
+            processor.update()
 
 class Item:
     def __init__(self, name, sell_in, quality):
@@ -62,3 +17,56 @@ class Item:
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
+class ItemProcessor:
+    def __init__(self, item):
+        self.item = item
+
+    def update(self):
+        self.update_quality()
+        self.update_sell_in()
+
+    def update_quality(self):
+        if self.item.quality > 0:
+            if self.item.sell_in > 0:
+                self.item.quality -= 1
+            else:
+                self.item.quality -= 2
+
+    def update_sell_in(self):
+        self.item.sell_in -= 1
+
+class AgedBrieProcessor(ItemProcessor):
+    def update_quality(self):
+        if self.item.quality < 50:
+            if self.item.sell_in > 0:
+                self.item.quality += 1
+            else:
+                self.item.quality += 2
+
+class BackstagePassProcessor(ItemProcessor):
+    def update_quality(self):
+        if self.item.quality < 50:
+            if self.item.sell_in <= 0:
+                self.item.quality = 0
+            elif self.item.sell_in <= 5:
+                self.item.quality += 3
+            elif self.item.sell_in <= 10:
+                self.item.quality += 2
+            else:
+                self.item.quality += 1
+
+class SulfurasProcessor(ItemProcessor):
+    def update(self):
+        pass
+
+class ProcessorFactory:
+    item_to_processor_mapping = {
+        "Aged Brie": AgedBrieProcessor,
+        "Backstage passes to a TAFKAL80ETC concert": BackstagePassProcessor,
+        "Sulfuras, Hand of Ragnaros": SulfurasProcessor,
+    }
+
+    @staticmethod
+    def for_item(item):
+        return ProcessorFactory.item_to_processor_mapping.get(item.name, ItemProcessor)(item)
