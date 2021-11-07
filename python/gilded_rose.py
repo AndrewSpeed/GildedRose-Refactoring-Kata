ff809a2c3696cd51d664f00262447b9bfd453df7
Andrew Speed <andrew.cspeed@gmail.com>
2021-11-07 15:24:31 +0000

    Add seam for Brie

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -5,11 +5,24 @@ class GildedRose(object):
     def __init__(self, items):
         self.items = items
 
+    def update_brie(self, item):
+        if item.quality < 50:
+            if item.sell_in > 0:
+                item.quality += 1
+            else:
+                item.quality += 2
+
+        item.sell_in -= 1
+        
+        return item
+
     def update_sulfuras(self, item):
         return item
 
     def update_quality(self):
         for item in self.items:
+            if item.name == "Aged Brie":
+                return self.update_brie(item)
             if item.name == "Sulfuras, Hand of Ragnaros":
                 return self.update_sulfuras(item)
             else:
